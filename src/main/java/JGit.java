e0b3906176e61cc3b04a0559741e6293a501e80d
dingrundong <dingrundong@kuaishou.com>
2022-06-30 18:10:59 +0800

    建立Maven项目

---
+++ src/main/java/JGit.java
@@ -0,0 +1,12 @@
+import org.eclipse.jgit.api.*;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.Test;
+
+
+public class JGit {
+}
